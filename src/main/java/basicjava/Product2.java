e2b33d92cbc26dc8a9d38e1735bc3c09e19fccc3
inaya-treva <samieajahaneva9543@gmail.com>
2025-06-02 13:32:20 +0600

    Done Assignment 2

---
+++ src/main/java/basicjava/Product2.java
@@ -0,0 +1,23 @@
+//Assignment = 2
+package basicjava;
+
+public class Product2 {
+    public static void main(String[] args){
+        int id;
+        String title, description, category, price_name;
+        double price;
+        id = 101;
+        title = "iphone15";
+        price = 1895;
+        price_name = "euros";
+        description = "perfect with best image quality";
+        category = "phone";
+        System.out.println("id : " + id +
+                "\ntitle : " + title +
+                "\nprice : " +price + price_name +
+                "\ndescription : " + description + 
+                "\ncategory = " + category
+        );
+    }
+}
+
